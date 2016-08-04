6fb3e03cbd2dd2df0634f3571b23fe8b0b8bb3dc
Go <gaojie@limei.com>
2016-08-04 18:21:31 +0800

    PoC

--- src/main/java/com/ueffort/study/example/PoC.java
+++ src/main/java/com/ueffort/study/example/PoC.java
@@ -41,15 +41,28 @@ public class PoC {
      * 输入一个字符串，打印出该字符串中字符的所有组合。
      *      例如输入字符串abc，则输出由字符a，b，c所能排列出来的所有字符串
      *      a, ab, abc, b, bc, c, ac
+     * 思路：
+     *      建立一个位图用于增量处理组合情况
+     *      位图的长度就是字符的数量，循环增1
+     *      通过判断当前数的每位的状态来显示字符，正好符合全组合的情况
      * Created by tutu on 16-8-4.
      */
     public static void combination(char[] s){
-
+        int t = 1 << s.length;
+        for(int i = 0; i < t; i++){
+            StringBuffer sb = new StringBuffer();
+            for(int j = 0; j < s.length; j++){
+                if((i & (1 << j)) > 0) sb.append(s[j]);
+            }
+            StdOut.println(sb.toString());
+        }
     }
 
     public static void main(String[] args){
         String s = StdIn.readLine();
+        StdOut.println("Permutation:");
         permutation(s.toCharArray(), 0, s.length());
-
+        StdOut.println("Conbination:");
+        combination(s.toCharArray());
     }
 }
